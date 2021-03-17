9a2b33d64265ff291bb6cb9c4cd2a4904d354be5
mcasiano1 <yssa_casiano@yahoo.com>
2021-03-16 22:50:08 -0500

    progress push

---
+++ climate_app.py
@@ -0,0 +1,12 @@
+# Import dependencies
+import pandas as pd
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+import datetime as dt
+
+# Import Flask
+from flask import Flask, redirect, jsonify
